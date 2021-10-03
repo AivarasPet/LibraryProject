2b5472343610a6cf6a1017a50af5a5c44cfd18b8
Aivaras Petrikas <aivaras.petrikas@dreamcubator.club>
2021-10-03 11:14:10 +0300

    Email and Password validators

--- src/com/company/Validators/EmailValidator.java
+++ src/com/company/Validators/EmailValidator.java
@@ -1,9 +1,48 @@
 package com.company.Validators;
 
+import com.company.Repositories.EmailDomainRepository;
+import com.company.Repositories.TldRepository;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class EmailValidator {
 
-    public boolean isEmailValid(String email) {
-        throw new UnsupportedOperationException("Not implemented");
+    private String forbiddenSymbols = ";][";
+    public boolean ContainsAtSymbol(String email) {
+        char ch;
+        for (int i = 0; i < email.length(); i++) {
+            ch = email.charAt(i);
+            if(ch == '@') return true;
+        }
+        return false;
+    }
+
+    public boolean ContainsForbiddenSymbols(String email) {
+        Set<Character> charsSet = forbiddenSymbols.chars()
+                .mapToObj(e->(char)e).collect(Collectors.toSet());
+
+        for (int i = 0; i < email.length(); i++) {
+            if(charsSet.contains(email.charAt(i))) return true;
+        }
+        return false;
+    }
+
+    public boolean HasGoodDomainName(String email) {
+        EmailDomainRepository emailDomainRepository = new EmailDomainRepository();
+        Set<String> domains = emailDomainRepository.getValidEmailDomains();
+        if(domains.contains(email.substring(email.indexOf('@')+1)))
+            return true;
+        return false;
+    }
+
+    public boolean HasGoodTLD(String email) {
+        TldRepository tldRepository = new TldRepository();
+        Set<String> availableTlds = tldRepository.getAvailableTlds();
+        String endOfEmail = email.substring(email.lastIndexOf('.'));
+        if(availableTlds.contains(endOfEmail))
+            return true;
+        return false;
     }
 
 }
--- src/com/company/Validators/PasswordChecker.java
+++ src/com/company/Validators/PasswordChecker.java
@@ -1,9 +1,47 @@
 package com.company.Validators;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class PasswordChecker {
 
-    public boolean isPasswordValid(String password, int expectedlength) {
-        throw new UnsupportedOperationException("Not implemented");
+    private String specialSymbols;
+
+    public PasswordChecker(String specialSymbols) {
+        this.specialSymbols = specialSymbols;
+    }
+
+    public boolean MeetsLengthRequirement(String password, int requiredCharacterCount) {
+        if(password.length() < requiredCharacterCount) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean HasUppercaseLetters(String password) {
+        char ch;
+        for(int i=0; i < password.length(); i++) {
+            ch = password.charAt(i);
+            if(ch <= 'Z' && ch >= 'A') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean HasSpecialSymbols(String password) {
+        // this string should have been a parameter
+        Set<Character> charsSet = specialSymbols.chars()
+                .mapToObj(e->(char)e).collect(Collectors.toSet());
+
+        char ch;
+        for(int i=0; i < password.length(); i++) {
+            ch = password.charAt(i);
+            if(charsSet.contains(ch)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
